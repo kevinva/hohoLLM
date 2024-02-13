8f306a4c48ba45322d47e84c00bd1cd958a84fd4
hoho <kevinvalentine@outlook.com>
2024-02-13 12:11:17 +0800

    test

--- FinGLM_test/load_data.py
+++ FinGLM_test/load_data.py
@@ -1,5 +1,6 @@
 from modelscope.msdatasets import MsDataset
 
-ds = MsDataset.load('chatglm_llm_fintech_raw_dataset', split='train', use_streaming=True, stream_batch_size=1, cache_dir = "/root/autodl-tmp/data/")
+
+ds =  MsDataset.load('chatglm_llm_fintech_raw_dataset', split='train', use_streaming=True, stream_batch_size=1, cache_dir = "/root/autodl-tmp/data/")
 for item in ds:
     print(item)
